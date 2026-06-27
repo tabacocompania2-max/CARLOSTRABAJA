1f235af576a099024ac03e15c468bf8073c5009f
tabacocompania2-max <tabacocompania2@gmail.com>
2026-06-27 17:10:14 -0400

    Add Microsoft Clarity tracking script

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -141,6 +141,18 @@ function RootShell({ children }: { children: ReactNode }) {
             alt=""
           />
         </noscript>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "xdrrfpsxjv");
+            `,
+          }}
+        />
       </head>
       <body>
         {children}
